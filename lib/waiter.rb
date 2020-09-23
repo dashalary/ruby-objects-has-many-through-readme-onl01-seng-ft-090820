d988769f4a5b4c0695bf9a49147eac58fd4263bf
Dasha Lary <dk2800@nyu.edu>
2020-09-23 19:35:10 +0000

    Automatically backed up by Learn

--- lib/waiter.rb
+++ lib/waiter.rb
@@ -17,6 +17,10 @@ attr_accessor :name, :yrs_experience
     Meal.new(self, customer, total, tip)
   end
   
-  
+  def meals
+  Meal.all.select do |meal|
+    meal.waiter == self
+  end
+end
   
 end
